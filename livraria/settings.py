2a47b8f72dcab35587dfd00957bc567ae7a5b6ff
RodrigoBonif <digoboni22@gmail.com>
2022-11-17 13:24:36 -0300

    Aula 14

--- livraria/settings.py
+++ livraria/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework_simplejwt",
     "rest_framework",
     'core',
 ]
@@ -44,6 +45,9 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.DjangoModelPermissions",
     ],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
 
 }
 
